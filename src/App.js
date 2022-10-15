1d7a899270cc99284015aab9e95a4c393b03f82c
Pablo Goeppinger <pgoeppin@gmail.com>
2022-10-15 08:44:58 -0300

    Adding 404 and redirects

--- src/App.js
+++ src/App.js
@@ -1,40 +1,33 @@
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import PizzaProvider from './context/PizzaContext';
-import Header from './components/Header';
-import Home from './views/Home';
-import Cart from './views/Cart';
-import PizzaDetalle from './views/PizzaDetalle';
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PizzaProvider from "./context/PizzaContext";
+import Header from "./components/Header";
+import Home from "./views/Home";
+import Cart from "./views/Cart";
+import PizzaDetalle from "./views/PizzaDetalle";
 
 function App() {
   return (
     <>
-    <PizzaProvider>
-    <BrowserRouter>
-      <Header/>
-      <Routes>
-          <Route
-            path="/"
-            element={
-                <Home />
-            }
-          />
-          <Route
-          path="/pizza/:pizzaId"
-          element={
-            <PizzaDetalle />
-          }
-          />
-          <Route
-          path="/cart"
-          element={
-            <Cart/>
-          }
-          />
-        </Routes>
-    </BrowserRouter>
-    </PizzaProvider>
+      <PizzaProvider>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/pizza/:pizzaId" element={<PizzaDetalle />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route
+              path="*"
+              element={
+                <h1 className="mt-5 text-center">
+                  404 Not Found. Porfavor, vuelve a inicio.
+                </h1>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </PizzaProvider>
     </>
   );
 }
